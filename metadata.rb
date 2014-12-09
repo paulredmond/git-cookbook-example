e0a7346a932f607caa8ae7f05e6d6ea22ee6ff78
Paul Redmond <paulrredmond@gmail.com>
2014-12-08 23:13:06 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'git-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures git-cookbook'
+long_description 'Installs/Configures git-cookbook'
+version          '0.1.0'
